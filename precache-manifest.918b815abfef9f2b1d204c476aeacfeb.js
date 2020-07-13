57dafc4d69215491e6bdd10056681a66f0ec06b2
Panacea <noach4se@gmail.com>
2020-07-13 14:02:27 +0200

    Updates

---
+++ precache-manifest.918b815abfef9f2b1d204c476aeacfeb.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b479054054ba870428d42966aa4d9e89",
+    "url": "/fond-pages/index.html"
+  },
+  {
+    "revision": "2a4e52e95161cef609af",
+    "url": "/fond-pages/static/css/2.cbdddf7c.chunk.css"
+  },
+  {
+    "revision": "3f7680e93c9c2bd7bdc7",
+    "url": "/fond-pages/static/css/main.8bc39894.chunk.css"
+  },
+  {
+    "revision": "2a4e52e95161cef609af",
+    "url": "/fond-pages/static/js/2.8a6da983.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/fond-pages/static/js/2.8a6da983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7680e93c9c2bd7bdc7",
+    "url": "/fond-pages/static/js/main.c013fb10.chunk.js"
+  },
+  {
+    "revision": "28e582b359460ef56698",
+    "url": "/fond-pages/static/js/runtime-main.4b35504b.js"
+  },
+  {
+    "revision": "452a301d0b07d28056b956ebad1ce8f5",
+    "url": "/fond-pages/static/media/about-us.452a301d.jpg"
+  },
+  {
+    "revision": "0957ef93cb628fec753d5fa49ffa9ea9",
+    "url": "/fond-pages/static/media/b1.0957ef93.jpg"
+  },
+  {
+    "revision": "22f54f5b5f17106e0f4a18cc7f3c272b",
+    "url": "/fond-pages/static/media/b2.22f54f5b.jpg"
+  },
+  {
+    "revision": "86056813993c68f3f1e6451b9bb53a65",
+    "url": "/fond-pages/static/media/b3.86056813.jpg"
+  },
+  {
+    "revision": "080e8b644a33acd592748aa96f7acfe3",
+    "url": "/fond-pages/static/media/book-lecture.080e8b64.jpg"
+  },
+  {
+    "revision": "fcc63798d483deb79d12cdd289e59730",
+    "url": "/fond-pages/static/media/c1.fcc63798.jpg"
+  },
+  {
+    "revision": "01888885396f545a829786a3877adb4a",
+    "url": "/fond-pages/static/media/c2.01888885.jpg"
+  },
+  {
+    "revision": "cdb433f7734b471c733f408629259eb6",
+    "url": "/fond-pages/static/media/c3.cdb433f7.jpg"
+  },
+  {
+    "revision": "09c59f6f98a696abd3fbd1a1eb8e7084",
+    "url": "/fond-pages/static/media/c4.09c59f6f.jpg"
+  },
+  {
+    "revision": "d7aec449b392f9c996b38d2e53a25a74",
+    "url": "/fond-pages/static/media/c5.d7aec449.jpg"
+  },
+  {
+    "revision": "45b6bd8f0d96761e288b2c8ba96ea948",
+    "url": "/fond-pages/static/media/c6.45b6bd8f.jpeg"
+  },
+  {
+    "revision": "5bea6eafea468fc7858ec6c432166c29",
+    "url": "/fond-pages/static/media/c7.5bea6eaf.jpg"
+  },
+  {
+    "revision": "8f7e09dde0c72c622e6d666a75f37bb5",
+    "url": "/fond-pages/static/media/l1.8f7e09dd.jpg"
+  },
+  {
+    "revision": "62f7cc8a0accd950f39f239e2cda72d3",
+    "url": "/fond-pages/static/media/l2.62f7cc8a.jpg"
+  },
+  {
+    "revision": "b34dc991b6a828e1c6be131f83fa09b4",
+    "url": "/fond-pages/static/media/l3.b34dc991.jpg"
+  },
+  {
+    "revision": "bf57840d7adeaefc28d3606a3bf1b088",
+    "url": "/fond-pages/static/media/l4.bf57840d.jpg"
+  },
+  {
+    "revision": "a92b5ef653f4aee79103a6913948161d",
+    "url": "/fond-pages/static/media/mak_green.a92b5ef6.jpg"
+  },
+  {
+    "revision": "47e178913b5d40e620e883f54642d0aa",
+    "url": "/fond-pages/static/media/opiat_mak.47e17891.jpg"
+  },
+  {
+    "revision": "b1a9cfd6c0e1d0cc2a7fed1bcf5a94cb",
+    "url": "/fond-pages/static/media/opiat_mak2.b1a9cfd6.jpg"
+  },
+  {
+    "revision": "bb20e5e6eea728b1513a1dd337ef7b49",
+    "url": "/fond-pages/static/media/original.bb20e5e6.jpg"
+  },
+  {
+    "revision": "a92cae014e0a9247239df340f08274dd",
+    "url": "/fond-pages/static/media/с8.a92cae01.jpg"
+  }
+]);
