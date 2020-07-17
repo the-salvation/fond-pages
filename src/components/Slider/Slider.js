a52d8a936be2a8f9ec53a8a65235a0252e5498c5
Krobirow <krobirow@gmail.com>
2020-07-17 21:04:33 +0200

    adaptive slider UPD

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -10,25 +10,25 @@ export default class Slider extends React.Component {
             <Carousel className={styles.slider}>
                 <Carousel.Item className={styles.sliderItem} >
                     <img
-                        className={`d-block w-100  ${ styles.imgItem}`}
+                        className={`d-block ${styles.imgItem}`}
                         src={bookLecture}
                         alt='book-a-lecture'
                     />
                     <Carousel.Caption>
-                        <h3>Замовити лекцію</h3>
-                        <p>Замовте лекцію зараз, це безкоштовно!</p>
+                        <h3 className={"text-danger"}>Замовити лекцію</h3>
+                        <p className={"text-danger"}>Замовте лекцію зараз, це безкоштовно!</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 
                 <Carousel.Item className={styles.sliderItem}>
                     <img
-                        className={`d-block w-100  ${ styles.imgItem}`}
+                        className={`d-block ${ styles.imgItem}`}
                         src={aboutUs}
                         alt='about-us'
                     />
                     <Carousel.Caption>
-                        <h3>Узнайте про нас больше!</h3>
-                        <p>фывфвыфыв</p>
+                        <h3 className={"text-danger"}>Узнайте про нас больше!</h3>
+                        <p className={"text-danger"}>фывфвыфыв</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
