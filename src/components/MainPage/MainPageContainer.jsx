d2c851f73c4fe4b5c2997873e7211efd2a1a0399
Panacea <noach4se@gmail.com>
2020-07-22 01:22:39 +0200

    made working version for release v1.2

--- src/components/MainPage/MainPageContainer.jsx
+++ src/components/MainPage/MainPageContainer.jsx
@@ -5,19 +5,23 @@ import style from './MainPage.module.scss'
 import RenderPlainText from '../Render/RenderPlainText/RenderPlainText';
 
 class MainPageContainer extends React.Component {
-	render(){
-		debugger;
+	debugger;
+	render() {
 		return (
-			<div className={style.sliderContainer}>
-				<MainPage {...this.props} />
-				<RenderPlainText {...this.props} partOfState={this.props.state.partOfState}/>
-				{/* <BookLecture partOfState={this.props.state.partOfState} /> */}
-			</div>
+				<div className={style.sliderContainer}>
+					<MainPage {...this.props} />
+				</div>
+			// <div className={style.MainPageContainerWrapper}>
+			// 	<div className={style.RenderPlainTextWrapper}>
+			// 		<RenderPlainText partOfState={this.props.state.partOfState} />
+			// 	</div>
+			// </div>
 		)
 	}
 }
 
 const mapStateToProps = (state) => ({
+	state: state.narcoticsReducer
 })
 
-export default connect(mapStateToProps, {}) (MainPageContainer);
+export default connect(mapStateToProps, {})(MainPageContainer);
