15ace76b49ef2d30539377640bd22e69165e447a
Panacea <noach4se@gmail.com>
2020-07-22 02:15:51 +0200

    Updates

---
+++ precache-manifest.5d0875baf107c1de18bbb08597ad3765.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485f5c74c81ba4ee7a4d8d4955c0731f",
+    "url": "/fond-pages/index.html"
+  },
+  {
+    "revision": "25d82109319126d27885",
+    "url": "/fond-pages/static/css/2.cbdddf7c.chunk.css"
+  },
+  {
+    "revision": "8a10ce7d64e16373c03e",
+    "url": "/fond-pages/static/css/main.26433c71.chunk.css"
+  },
+  {
+    "revision": "25d82109319126d27885",
+    "url": "/fond-pages/static/js/2.0fe226ad.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/fond-pages/static/js/2.0fe226ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a10ce7d64e16373c03e",
+    "url": "/fond-pages/static/js/main.ce58b523.chunk.js"
+  },
+  {
+    "revision": "28e582b359460ef56698",
+    "url": "/fond-pages/static/js/runtime-main.4b35504b.js"
+  },
+  {
+    "revision": "b92e0a67bed42b5f16a3fae2a142580f",
+    "url": "/fond-pages/static/media/a1.b92e0a67.jpg"
+  },
+  {
+    "revision": "a033e16fe7d6b09721c2c13fd889c4ef",
+    "url": "/fond-pages/static/media/a2.a033e16f.jpg"
+  },
+  {
+    "revision": "e42f4ddae3d4eb887e5291b1b6ecce37",
+    "url": "/fond-pages/static/media/a3.e42f4dda.jpg"
+  },
+  {
+    "revision": "29c30942fe4eb3ac9b969c4c3efa3f8f",
+    "url": "/fond-pages/static/media/a4.29c30942.jpg"
+  },
+  {
+    "revision": "b07d96040be8d3079b8202f93f49c341",
+    "url": "/fond-pages/static/media/a5.b07d9604.jpg"
+  },
+  {
+    "revision": "20f677dd6807cb4a9f4fd41f2e39b909",
+    "url": "/fond-pages/static/media/a6.20f677dd.jpg"
+  },
+  {
+    "revision": "1fd01714de4554f2cb5e93a0383ef693",
+    "url": "/fond-pages/static/media/a7.1fd01714.jpg"
+  },
+  {
+    "revision": "b8ed300b2efa5420be94d797a33b97e0",
+    "url": "/fond-pages/static/media/a8.b8ed300b.jpg"
+  },
+  {
+    "revision": "452a301d0b07d28056b956ebad1ce8f5",
+    "url": "/fond-pages/static/media/about-us.452a301d.jpg"
+  },
+  {
+    "revision": "0957ef93cb628fec753d5fa49ffa9ea9",
+    "url": "/fond-pages/static/media/b1.0957ef93.jpg"
+  },
+  {
+    "revision": "22f54f5b5f17106e0f4a18cc7f3c272b",
+    "url": "/fond-pages/static/media/b2.22f54f5b.jpg"
+  },
+  {
+    "revision": "86056813993c68f3f1e6451b9bb53a65",
+    "url": "/fond-pages/static/media/b3.86056813.jpg"
+  },
+  {
+    "revision": "080e8b644a33acd592748aa96f7acfe3",
+    "url": "/fond-pages/static/media/book-lecture.080e8b64.jpg"
+  },
+  {
+    "revision": "fcc63798d483deb79d12cdd289e59730",
+    "url": "/fond-pages/static/media/c1.fcc63798.jpg"
+  },
+  {
+    "revision": "01888885396f545a829786a3877adb4a",
+    "url": "/fond-pages/static/media/c2.01888885.jpg"
+  },
+  {
+    "revision": "cdb433f7734b471c733f408629259eb6",
+    "url": "/fond-pages/static/media/c3.cdb433f7.jpg"
+  },
+  {
+    "revision": "09c59f6f98a696abd3fbd1a1eb8e7084",
+    "url": "/fond-pages/static/media/c4.09c59f6f.jpg"
+  },
+  {
+    "revision": "d7aec449b392f9c996b38d2e53a25a74",
+    "url": "/fond-pages/static/media/c5.d7aec449.jpg"
+  },
+  {
+    "revision": "45b6bd8f0d96761e288b2c8ba96ea948",
+    "url": "/fond-pages/static/media/c6.45b6bd8f.jpeg"
+  },
+  {
+    "revision": "5bea6eafea468fc7858ec6c432166c29",
+    "url": "/fond-pages/static/media/c7.5bea6eaf.jpg"
+  },
+  {
+    "revision": "bd086cafa860c76644e4f596406c2237",
+    "url": "/fond-pages/static/media/d1.bd086caf.jpg"
+  },
+  {
+    "revision": "5d616b1604d43d65cd9f882d8313d15a",
+    "url": "/fond-pages/static/media/dmaa1.5d616b16.jpg"
+  },
+  {
+    "revision": "2858cafd0c0d396f4969a69b490dab31",
+    "url": "/fond-pages/static/media/e1.2858cafd.jpg"
+  },
+  {
+    "revision": "5251f9dd5e74a0ce2b1370ec92a98ab0",
+    "url": "/fond-pages/static/media/e4.5251f9dd.jpg"
+  },
+  {
+    "revision": "06a84e93a79bfb30a8afa79cbd7b31e5",
+    "url": "/fond-pages/static/media/k1.06a84e93.jpg"
+  },
+  {
+    "revision": "1c44ea59f49d2223bf2953d05a132e4b",
+    "url": "/fond-pages/static/media/k2.1c44ea59.jpeg"
+  },
+  {
+    "revision": "40aeaa4db793874f89d514bd8d370275",
+    "url": "/fond-pages/static/media/k3.40aeaa4d.jpg"
+  },
+  {
+    "revision": "91569b9a5da09481ff0960ca8c00a639",
+    "url": "/fond-pages/static/media/k4.91569b9a.jpg"
+  },
+  {
+    "revision": "a2d1c83379e76b1d7859cdf68f5a36a5",
+    "url": "/fond-pages/static/media/k5.a2d1c833.jpg"
+  },
+  {
+    "revision": "8f7e09dde0c72c622e6d666a75f37bb5",
+    "url": "/fond-pages/static/media/l1.8f7e09dd.jpg"
+  },
+  {
+    "revision": "62f7cc8a0accd950f39f239e2cda72d3",
+    "url": "/fond-pages/static/media/l2.62f7cc8a.jpg"
+  },
+  {
+    "revision": "b34dc991b6a828e1c6be131f83fa09b4",
+    "url": "/fond-pages/static/media/l3.b34dc991.jpg"
+  },
+  {
+    "revision": "bf57840d7adeaefc28d3606a3bf1b088",
+    "url": "/fond-pages/static/media/l4.bf57840d.jpg"
+  },
+  {
+    "revision": "a92b5ef653f4aee79103a6913948161d",
+    "url": "/fond-pages/static/media/mak_green.a92b5ef6.jpg"
+  },
+  {
+    "revision": "47e178913b5d40e620e883f54642d0aa",
+    "url": "/fond-pages/static/media/opiat_mak.47e17891.jpg"
+  },
+  {
+    "revision": "b1a9cfd6c0e1d0cc2a7fed1bcf5a94cb",
+    "url": "/fond-pages/static/media/opiat_mak2.b1a9cfd6.jpg"
+  },
+  {
+    "revision": "bb20e5e6eea728b1513a1dd337ef7b49",
+    "url": "/fond-pages/static/media/original.bb20e5e6.jpg"
+  },
+  {
+    "revision": "f3f29e4d1718470186814a97cbcd6947",
+    "url": "/fond-pages/static/media/t1.f3f29e4d.jpg"
+  },
+  {
+    "revision": "ffeac38964607d770870b2a3b039d05e",
+    "url": "/fond-pages/static/media/t2.ffeac389.jpg"
+  },
+  {
+    "revision": "a92cae014e0a9247239df340f08274dd",
+    "url": "/fond-pages/static/media/с8.a92cae01.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c017b3e02551b0eecdf31cc32f417576.js"
+  "/fond-pages/precache-manifest.5d0875baf107c1de18bbb08597ad3765.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fond-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
