837006a4d6e2d431b9f7bccd6bfd7bacdfd427d0
Krobirow <krobirow@gmail.com>
2020-08-03 13:08:03 +0200

    search upd

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,84 +1,98 @@
 import React, { Component, Fragment } from 'react';
-import styles from './Search.module.scss'
+// import styles from './Search.scss'
 
 
 export default class Search extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.overlay1 = React.createRef() 
-    //     this.overlay2 = React.createRef() 
-    //     this.search = React.createRef() 
-    //     this.input = React.createRef() 
-    // }
+    constructor(props) {
+        super(props);
+        this.SearchRef =  React.createRef();
+        this.InputRef =  React.createRef();
+    }
+    
+    componentDidMount() {
+        document.body.addEventListener('click', this.bodyListenerRemoveSearch.bind(this));
+    }
+    componentWillUnmount() {
+        console.log('i was unmounted');
+        document.body.removeEventListener('click', this.bodyListenerRemoveSearch.bind(this));
+    }
 
-    // componentDidMount() {
-    //     const overlay1 = document.querySelector('.overlay1');
-    //     const overlay2 = document.querySelector('.overlay2');
-    //     const search = document.querySelector('.search');
-    //     const input = document.querySelector('.input');
-    //     debugger
-    //     overlay1.addEventListener('click', () => {
-    //         search.classList.toggle('active');
-    //         if (search.classList.contains('active')) {
-    //             setTimeout(() => {
-    //                 input.focus();
-    //             }, 200)
-    //         }
-    //     })
-    //     search.addEventListener('click', () => {
-    //         if (search.classList.contains('active')) {
-    //             setTimeout(() => {
-    //                 input.focus();
-    //             }, 200)
-    //         }
-    //     })
-    //     overlay2.addEventListener('click', (e) => {
-    //         input.value = '';
-    //         input.focus();
-    //         search.classList.remove('searching')
-    //     })
-    //     document.body.addEventListener('click', (e) => {
-    //         if (!search.contains(e.target) && input.value.length === 0) {
-    //             search.classList.remove('active');
-    //             search.classList.remove('searching');
-    //             input.value = '';
-    //         }
-    //     })
-    //     input.addEventListener('keyup', (e) => {
-    //         if (e.keyCode === 13) {
-    //             input.blur();
-    //         }
-    //     })
-    //     input.addEventListener('input', () => {
-    //         if (input.value.length > 0) {
-    //             search.classList.add('searching')
-    //         } else {
-    //             search.classList.remove('searching')
-    //         }
-    //     })
-    //     input.value = '';
-    //     input.blur();
-    // }
+    inputFocus() {
+        const search = this.SearchRef.current;
+        const input = this.InputRef.current;
+        search.classList.toggle('active');
+        if (search.classList.contains('active')) {
+            setTimeout(() => {
+                input.focus();
+            }, 200)
+        }
+    }
+    searchFocus() {
+        const search = this.SearchRef.current;
+        const input = this.InputRef.current;
+        if (search.classList.contains('active')) {
+            setTimeout(() => {
+                input.focus();
+            }, 200)
+        }
+    }
+    searchRemove() {
+        const search = this.SearchRef.current;
+        const input = this.InputRef.current;
+        input.value = '';
+        input.focus();
+        search.classList.remove('searching')
+    }
+
+    searchHide(event) {
+        const search = this.SearchRef.current;
+        const input = this.InputRef.current;
+        if (!search.contains(event.target) && input.value.length === 0) {
+        search.classList.remove('active');
+        search.classList.remove('searching');
+        input.value = '';
+        }
+    }
+
+    toggleSearch(e){
+        const search = this.SearchRef.current;
+        if (e.target.value.length > 0) {
+            search.classList.add(`searching`)
+        } else {
+            search.classList.remove(`searching`)
+        }
+    }
+
+    bodyListenerRemoveSearch(e) {
+        const search = this.SearchRef.current;
+        const input = this.InputRef.current;
+        if (!search.contains(e.target) && input.value.length === 0) {
+                search.classList.remove('active');
+                search.classList.remove('searching');
+                input.value = '';
+            }
+    }
 
     render() {
 
         return (
-            <div className={styles.input} type="text">
-
-                <input className={styles.input} type="text" />
-                <svg viewBox="0 0 700 100" className={styles.magnifyingGlass}>
-                    <path
-                        className={styles.magnifyingGlassPath}
-                        d="m 59.123035,59.123035 c -10.561361,10.56136 -27.684709,10.56136 -38.24607,0 -10.56136,-10.561361 -10.56136,-27.684709 0,-38.24607 10.561361,-10.56136 27.684709,-10.56136 38.24607,0 10.56136,10.561361 10.56136,27.684709 0,38.24607 l 28.876965,28.876965 c 6.304625,7.101523 5.754679,-0.187815 13.07143,-0.5 h 582.04101" />
-                    <path
-                        className={styles.x}
-                        d="m 673.46803,25.714286 -37.17876,38.816532 c 0,0 -5.08857,5.60515 -5.68529,11.841734 -1.06622,11.143538 13.02902,11.127448 13.02902,11.127448" />
-                    <path
-                        className={styles.x}
-                        d="m 635.08021,25.714286 37.17876,38.816532 c 0,0 5.08857,5.60515 5.68529,11.841734 1.06622,11.143538 -13.02902,11.127448 -13.02902,11.127448" />
-                </svg>
-                <div className={styles.overlay1}></div>
-                <div className={styles.overlay2}></div>
+            <div className={'searchWrapper'} onClick={(event) => {this.searchHide(event)}}>
+                <div className={'search'} ref={this.SearchRef} onClick={() => {this.searchFocus()}} type="text">
+                    <input ref={this.InputRef} className={'input'} type="text" onChange={(event) => { this.toggleSearch(event)}} />
+                    <svg viewBox="0 0 700 100" className={'magnifyingGlass'}>
+                        <path
+                            className={'magnifyingGlassPath'}
+                            d="m 59.123035,59.123035 c -10.561361,10.56136 -27.684709,10.56136 -38.24607,0 -10.56136,-10.561361 -10.56136,-27.684709 0,-38.24607 10.561361,-10.56136 27.684709,-10.56136 38.24607,0 10.56136,10.561361 10.56136,27.684709 0,38.24607 l 28.876965,28.876965 c 6.304625,7.101523 5.754679,-0.187815 13.07143,-0.5 h 582.04101" />
+                        <path
+                            className={'x'}
+                            d="m 673.46803,25.714286 -37.17876,38.816532 c 0,0 -5.08857,5.60515 -5.68529,11.841734 -1.06622,11.143538 13.02902,11.127448 13.02902,11.127448" />
+                        <path
+                            className={'x'}
+                            d="m 635.08021,25.714286 37.17876,38.816532 c 0,0 5.08857,5.60515 5.68529,11.841734 1.06622,11.143538 -13.02902,11.127448 -13.02902,11.127448" />
+                    </svg>
+                    <div className={'overlay' + " " + 'overlay1'} onClick={() => {this.inputFocus()}}></div>
+                    <div className={'overlay' + " " + 'overlay2'} onClick={() => {this.searchRemove()}}></div>
+                </div>
             </div>
         );
     }
