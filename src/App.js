104433a1a099e80d8866ab824d4add36856d0fb5
Krobirow <bodia122@gmail.com>
2020-12-10 21:14:02 +0100

    project without express UPD

--- src/App.js
+++ src/App.js
@@ -74,31 +74,8 @@ const App123 = (props) => {
 };
 
 
-class App extends Component {
-    
-    state = {
-        data: null
-    };
-
-    componentDidMount() {
-        // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-        .then(res => this.setState({ data: res.express }))
-        .catch(err => console.log(err));
-    }
-        // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-    callBackendAPI = async () => {
-        const response = await fetch('/express_backend');
-        const body = await response.json();
-
-        if (response.status !== 200) {
-            throw Error(body.message) 
-        }
-        return body;
-    };
-    
+class App extends Component {  
     render() {
-        console.log(this.state.data);
         return (
             <BrowserRouter>
                 <div className={classes.app}>
@@ -152,7 +129,6 @@ class App extends Component {
                     <BrowserView viewClassName={classes.browserFooter}>
                         <Footer/>
                     </BrowserView>
-                    <p className="App-intro">{this.state.data}</p>
                 </div>
             </BrowserRouter>
         );
