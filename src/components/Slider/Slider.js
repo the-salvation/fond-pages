4faefd79b53bc28b931c4c21d5a9e62682ee3b6b
Panacea <noach4se@gmail.com>
2020-12-20 03:08:06 +0200

    added new links and some padding

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,17 +3,20 @@ import bookLecture from "./../../assets/images/slider/book-lecture.jpg";
 import aboutUs from "./../../assets/images/slider/about-us.jpg";
 import styles from './Slider.module.scss';
 import {Carousel} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export default class Slider extends React.Component {
     render() {
         return (
             <Carousel className={styles.slider}>
                 <Carousel.Item className={styles.sliderItem} >
+                    {<NavLink exact to="/book-a-lecture">
                     <img
                         className={`d-block ${styles.imgItem}`}
                         src={bookLecture}
                         alt='book-a-lecture'
                     />
+                    </NavLink>}
                     <Carousel.Caption>
                         <h3 className={"text-danger"}> </h3>
                         <p className={"text-danger"}> </p>
