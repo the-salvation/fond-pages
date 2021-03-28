a90a189b5764a553919f11d6803728de1f451ba8
Krobirow <bodia122@gmail.com>
2021-03-28 15:49:49 +0200

    FW-2 proptypes implemented || FAQPage upd with propTypes

--- src/components/FaqPageContainer/FaqPageContainer.jsx
+++ src/components/FaqPageContainer/FaqPageContainer.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import style from './FaqPage.module.scss'
 import RenderPlainText from '../Render/RenderPlainText/RenderPlainText';
 
 class FaqPageContainer extends React.Component {
 	render() {
 		return (
-				<div className={style.container}>
-					<RenderPlainText partOfState={this.props.store.FAQ} />
-				</div>
-			
-		)
+			<div className={style.container}>
+				<RenderPlainText partOfState={this.props.faqPageData.FAQ} />
+			</div>
+		);
 	}
 }
 
 const mapStateToProps = (state) => ({
-	state: state.narcoticsReducer
-})
+	faqPageData: state.narcoticsReducer
+});
+
+FaqPageContainer.propTypes = {
+	faqPageData: PropTypes.object
+};
 
 export default connect(mapStateToProps, {})(FaqPageContainer);
