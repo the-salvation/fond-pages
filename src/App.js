672af848bff111e16ccc8cfd8859f7c515c6f666
Eugene Markelov <noach4se@gmail.com>
2021-03-28 20:56:45 +0300

    eslint fixes completed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Toggle from "./Common/Preloader/Toggle/Toggle";
 import NarcotisContainer from "./components/Narcotics/NarcoticasContainer";
 import store from "./redux/state";
 import BookLectureContainer from "./components/BookLectureContainer/BookLectureContainer";
-import { BrowserView,MobileView} from "react-device-detect";
+import { BrowserView, MobileView } from "react-device-detect";
 import NavBurger from "./components/NavBurger/NavBurger";
 import FaqPageContainer from "./components/FaqPageContainer/FaqPageContainer";
 import ConsultPageContainer from "./components/ConsultPageContainer/ConsultPageContainer";
@@ -64,7 +64,7 @@ import ConsultPageContainer from "./components/ConsultPageContainer/ConsultPageC
 //                         </Switch>
 //                     </div>
 //                 </div>
-                
+
 //                 <BrowserView viewClassName={classes.browserFooter}>
 //                     <Footer/>
 
@@ -74,70 +74,68 @@ import ConsultPageContainer from "./components/ConsultPageContainer/ConsultPageC
 //     );
 // };
 
-
-class App extends Component {  
-    render() {
-        return (
-            <BrowserRouter>
-                <div className={classes.app}>
-                    <BrowserView viewClassName={classes.app_wrapper_header}>
-                        {/* <div className={classes.app_wrapper_header}>
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className={classes.app}>
+          <BrowserView viewClassName={classes.app_wrapper_header}>
+            {/* <div className={classes.app_wrapper_header}>
                             <Header />
                         </div> */}
-                            <Header />
-                    </BrowserView>
-                    <div className={classes.app_wrapper}>
-                        <div className={classes.app_wrapper_nav}>
-                            <MobileView viewClassName={classes.mobileBurger}>
-                                <NavBurger store={store}/>
-                            </MobileView>
-                            <BrowserView viewClassName={classes.browserNav}>
-                                <Nav store={store} />
-                                <Toggle
-                                    render={({ on, Toggle }) => (
-                                        <div className={classes.app_wrapper_toggle}>
-                                            {/* <button onClick={Toggle}>k</button> */}
-                                            {on && <Nav />}
-                                        </div>
-                                    )}
-                                />
-                            </BrowserView>
-                        </div>
-                        <div className={classes.app_wrapper_content}>
-                            <Switch>
-                                <Route
-                                    path="/main"
-                                    render={() => <MainPageContainer store={store} />}
-                                />
-                                <Route
-                                    path="/faq"
-                                    render={() => <FaqPageContainer store={store} />}
-                                />
-                                <Route
-                                    path="/get-consultation"
-                                    render={() => <ConsultPageContainer store={store} />}
-                                />
-                                <Route
-                                    path="/narcotics"
-                                    render={() => <NarcotisContainer store={store} />}
-                                />
-                                <Route
-                                    path="/book-a-lecture"
-                                    render={() => <BookLectureContainer store={store} />}
-                                />
-                                <Route render={() => <MainPageContainer store={store} />}
-                                />
-                            </Switch>
-                        </div>
+            <Header />
+          </BrowserView>
+          <div className={classes.app_wrapper}>
+            <div className={classes.app_wrapper_nav}>
+              <MobileView viewClassName={classes.mobileBurger}>
+                <NavBurger store={store} />
+              </MobileView>
+              <BrowserView viewClassName={classes.browserNav}>
+                <Nav store={store} />
+                <Toggle
+                  render={({ on, Toggle }) => (
+                    <div className={classes.app_wrapper_toggle}>
+                      {/* <button onClick={Toggle}>k</button> */}
+                      {on && <Nav />}
                     </div>
-                    
-                    <BrowserView viewClassName={classes.browserFooter}>
-                        <Footer/>
-                    </BrowserView>
-                </div>
-            </BrowserRouter>
-        );
-    }
-};
+                  )}
+                />
+              </BrowserView>
+            </div>
+            <div className={classes.app_wrapper_content}>
+              <Switch>
+                <Route
+                  path="/main"
+                  render={() => <MainPageContainer store={store} />}
+                />
+                <Route
+                  path="/faq"
+                  render={() => <FaqPageContainer store={store} />}
+                />
+                <Route
+                  path="/get-consultation"
+                  render={() => <ConsultPageContainer store={store} />}
+                />
+                <Route
+                  path="/narcotics"
+                  render={() => <NarcotisContainer store={store} />}
+                />
+                <Route
+                  path="/book-a-lecture"
+                  render={() => <BookLectureContainer store={store} />}
+                />
+                <Route render={() => <MainPageContainer store={store} />} />
+              </Switch>
+            </div>
+          </div>
+
+          <BrowserView viewClassName={classes.browserFooter}>
+            <Footer />
+          </BrowserView>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
