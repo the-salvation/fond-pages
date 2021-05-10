12d86dce4a2d8b77de13a0d2a80d4a22f7e131c1
Krobirow <bodia122@gmail.com>
2021-05-10 21:01:59 +0200

    FW-6 booLectureContainer fixed

--- src/components/BookLectureContainer/BookLectureContainer.js
+++ src/components/BookLectureContainer/BookLectureContainer.js
@@ -3,42 +3,18 @@ import style from './BookLectureContainer.module.scss'
 import { connect } from 'react-redux';
 import RenderPlainText from '../Render/RenderPlainText/RenderPlainText';
 import PropTypes from 'prop-types';
-// import BookLectureForm from '../Forms/BookLectureForm/BookLectureForm';
-// import MyEmail from './Email.js_';
+import BookLectureForm from '../Forms/BookLectureForm/BookLectureForm';
 
 class BookLectureContainer extends Component {
-    // {this.props.state.partOfState.id === "Opium" ? <OneNarcotic partOfState={this.props.state.partOfState}/> : null }
-    // onSubmit = (formData) => {
-
-    //     const messageHtml =  renderEmail(<MyEmail formData={{...formData}} />);
-    //     // console.log(formData);
-
-    //     axios({
-    //         method: "POST", 
-    //         url:"https://www.fondnn.org.ua/book-a-lecture/send", 
-    //         data: {
-    //             name: formData.firstName,
-    //             email: formData.email,
-    //             messageHtml: messageHtml
-    //         }
-    //     }).then((response)=>{
-    //         if (response.data.msg === 'success'){
-    //             console.log("Email sent, awesome!"); 
-    //             // this.resetForm()
-    //         }else if(response.data.msg === 'fail'){
-    //             console.log("Oops, something went wrong. Try again")
-    //         }
-    //     })
-    // }
-
-    // };
-
+    onSubmit = (formData) => {
+        console.log(formData);
+    }
 
     render() {
         return (
             <div className={style.bookLectureContainer}>
                 <RenderPlainText {...this.props.bookLectureState.partOfState} />
-                {/* <BookLectureForm form='bookLecture' onSubmit={this.onSubmit}/> */}
+                <BookLectureForm form='bookLecture' onSubmit={this.onSubmit}/>
             </div>
         )
     }
