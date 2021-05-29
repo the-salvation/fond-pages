f5f467fd03b5c44f0e6258a347c8efc54e023f11
Krobirow <bodia122@gmail.com>
2021-05-29 11:22:51 +0200

    FW-7 navlink refactored

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -17,7 +17,10 @@ import PropTypes from 'prop-types';
 class Nav extends React.Component {
 
   render() {
-    const { setNarcoticAC } = this.props;
+    const { setNarcoticAC, allNarcotics } = this.props;
+
+    console.log(allNarcotics, " allNarcotics in NAV");
+
     return (
       <nav className={classes.nav}>
         <div className={`${classes.item} ${classes.active}`}>
@@ -40,107 +43,34 @@ class Nav extends React.Component {
             {
               on &&
               <div className={classes.item}>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/narcotics" onClick={() => { setNarcoticAC("Narcotics"); }} activeClassName={classes.active}>Дія&nbsp;наркотиків</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/konopli" onClick={() => { setNarcoticAC("Konopli"); }} activeClassName={classes.active}>Коноплі</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/opium" onClick={() => { setNarcoticAC("Opium"); }} activeClassName={classes.active}>Опіум</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/barbiturati" onClick={() => { setNarcoticAC("Barbiturati"); }} activeClassName={classes.active}>Барбітурати</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/salt" onClick={() => { setNarcoticAC("Salt"); }} activeClassName={classes.active}>Сіль</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/lsd" onClick={() => { setNarcoticAC("Lsd"); }} activeClassName={classes.active}>ЛСД</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/cocaine" onClick={() => { setNarcoticAC("Cocaine"); }} activeClassName={classes.active}>КокаЇн</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/heroine" onClick={() => { setNarcoticAC("Heroine"); }} activeClassName={classes.active}>Героїн</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/ketamine" onClick={() => { setNarcoticAC("Ketamine"); }} activeClassName={classes.active}>Кетамін</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/durman" onClick={() => { setNarcoticAC("Durman"); }} activeClassName={classes.active}>Дурман</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/dmaa" onClick={() => { setNarcoticAC("DMAA"); }} activeClassName={classes.active}>DMAA</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/mdma" onClick={() => { setNarcoticAC("MDMA"); }} activeClassName={classes.active}>MDMA Екстазі</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/alcohol" onClick={() => { setNarcoticAC("Alcohol"); }} activeClassName={classes.active}>Алкоголь</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/tramadol" onClick={() => { setNarcoticAC("Tramadol"); }} activeClassName={classes.active}>Трамадол</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/toxicomany" onClick={() => { setNarcoticAC("Toxic"); }} activeClassName={classes.active}>Токсикоманія</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/ephedrin" onClick={() => { setNarcoticAC("Ephedrin"); }} activeClassName={classes.active}>Ефедрин</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/morphine" onClick={() => { setNarcoticAC("Morphine"); }} activeClassName={classes.active}>Морфін</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/mak-snodiyniy" onClick={() => { setNarcoticAC("Poppy"); }} activeClassName={classes.active}>Мак снодійний</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/pervitin" onClick={() => { setNarcoticAC("Pervitin"); }} activeClassName={classes.active}>Первітин</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/meskalin" onClick={() => { setNarcoticAC("Meskalin"); }} activeClassName={classes.active}>Мескалін</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/psylocibe" onClick={() => { setNarcoticAC("Psylocibe"); }} activeClassName={classes.active}>Псилоцибін</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/fenciklidin" onClick={() => { setNarcoticAC("Fen"); }} activeClassName={classes.active}>Фенциклідин</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/codeine" onClick={() => { setNarcoticAC("Codeine"); }} activeClassName={classes.active}>Кодеїн</NavLink>
-                </div>
-                <div className={`${classes.itemNarko}`}>
-                  <NavLink to="/narcotics/methadone" onClick={() => { setNarcoticAC("Methadone"); }} activeClassName={classes.active}>Метадон</NavLink>
-                </div>
-
-                {/* {this.navs} */}
-
+                {
+                  allNarcotics.map(narcotic => {
+                    return (
+                      <div key={narcotic.name} className={`${classes.itemNarko}`}>
+                        <NavLink to={`/narcotics/${narcotic.name}`} onClick={() => { setNarcoticAC(narcotic.name); }} activeClassName={classes.active}>{narcotic.descr}</NavLink>
+                      </div>
+                    )
+                  })
+                }
               </div>
             }
           </div>
         )}
         />
         <br />
-
       </nav>
     );
 
   }
 }
 
-// export default Nav;
-
-// let mapStateToProps = (state) => ({
-//   state: state
-// });
-
 Nav.propTypes = {
-  setNarcoticAC: PropTypes.string
+  setNarcoticAC: PropTypes.func
 };
 
 export default connect(
   state => ({
-    // : state.narcoticsReducer
+    allNarcotics : state.narcoticsReducer.allNarcotics
   }),
   dispatch => {
     return bindActionCreators({
